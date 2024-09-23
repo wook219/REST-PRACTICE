18c938894ba0cdaa1f171b82f1195fd9d434aaf6
[CLOUD4] 송진욱 <wlsdnr19731@gmail.com>
2024-09-23 16:57:19 +0900

    feat : 로깅을 위한 Slf4j 어노테이션 추가 및 create, retrieve 기능 비즈니스 로직 추가

--- src/main/java/com/example/rest_practice/service/TodoService.java
+++ src/main/java/com/example/rest_practice/service/TodoService.java
@@ -2,9 +2,13 @@ package com.example.rest_practice.service;
 
 import com.example.rest_practice.model.TodoEntity;
 import com.example.rest_practice.persistence.TodoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Slf4j
 @Service
 public class TodoService {
 
@@ -17,5 +21,38 @@ public class TodoService {
 
         TodoEntity savedEntity = todoRepository.findById(todoEntity.getId()).get();
         return savedEntity.getTitle();
+    }
+
+    //create
+    public List<TodoEntity> create(final TodoEntity todoEntity){
+        //Validations
+        validate(todoEntity);
+
+        // 엔티티를 DB에 저장 후 로그를 남긴다.
+        todoRepository.save(todoEntity);
+        log.info("Entity Id : {} is saved", todoEntity.getId());
+
+        //저장된 엔티티를 포함하는 새 리스트를 리턴한다.
+        return todoRepository.findByUserId(todoEntity.getUserId());
+    }
+
+    //retrieve
+    public List<TodoEntity> retrieve(final String userId){
+        log.info("result of Entity userId : {}", userId);
+
+        return todoRepository.findByUserId(userId);
+    }
+
+
+    private void validate(TodoEntity todoEntity) {
+        if(todoEntity == null){
+            log.warn("Entity cannot be null.");
+            throw new RuntimeException("Entity cannot be null.");
+        }
+
+        if(todoEntity.getUserId() == null){
+            log.warn("Unknown User.");
+            throw new RuntimeException("Unknown User.");
+        }
     }
 }
